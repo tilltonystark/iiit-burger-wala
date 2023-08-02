d162720547540516b5d5ac42d4339a30d5a9129c
Satyam Gopal <tilltonystark@gmail.com>
2023-08-03 05:24:24 +0530

    login UI updated

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {FaGoogle} from 'react-icons/fa'
 
 const Login = () => {
-    return (
-    <div>Login</div>
-    )
+    return <section className='login'>
+        <button> <FaGoogle/> Login with Google</button>
+    </section>
 }
 
 export default Login
